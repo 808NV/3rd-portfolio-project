a7f0ce1cbca64b73ee25e2c6e0b8779badb1454d
omar <yourname@example.com>
2024-05-04 09:46:47 +0300

    Refactor: Replace static layout with dynamic routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import Navbar from "./components/navbar/Navbar";
-import "./layout.scss";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./app.scss";
+import Layout from "./components/layout/Layout";
 import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="list" element={<List />} />
+    </Route>
+  )
+);
 function App() {
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <Home />
-      </div>
+    <div className="app">
+      <RouterProvider router={router} />
     </div>
   );
 }
