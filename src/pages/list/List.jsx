38da50fec818fd60ee211c14980db866f0d89e34
omar <yourname@example.com>
2024-05-04 09:50:35 +0300

    Feature: Implement List component with Filter, Card rendering, and Map display

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,26 @@
+import Filter from "../../components/filter/Filter";
+import "./list.scss";
+import { listData } from "../../lib/dummyData";
+import Card from "../../components/card/Card";
+import Map from "../../components/map/Map";
+
+const List = () => {
+  return (
+    <div className="list">
+      <div className="cards">
+        <div className="wrapper">
+          <Filter />
+
+          {listData.map((item) => {
+            return <Card key={item.id} item={item} />;
+          })}
+        </div>
+      </div>
+      <div className="map">
+        <Map items={listData} />
+      </div>
+    </div>
+  );
+};
+
+export default List;
