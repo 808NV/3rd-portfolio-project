6f27405e5ab7aa7f4eb527c514b654b70f3803b4
omar <yourname@example.com>
2024-05-11 10:15:16 +0300

    mplement user registration and authentication endpoints

--- api/app.js
+++ api/app.js
@@ -1,14 +1,17 @@
 import express from "express";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const port = 5000;
 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/posts", postRoute);
-
 app.use("/api/auth", authRoute);
 
 app.listen(port, () => {
